6756ad8ae53ffc7e44bf97504f14c07f26f48c80
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-04-22 14:48:16 -0400

    Initial Commit

---
+++ HarryPotterInfo/HarryPotterInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterInfoApp.swift
+//  HarryPotterInfo
+//
+//  Created by Kiho Okazawa on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
